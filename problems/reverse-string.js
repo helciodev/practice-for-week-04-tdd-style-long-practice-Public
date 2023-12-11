bf56cc8fa442ddc76ed4b2bb971bec1a1f6cd07d
helcio <h.andre@giap.pl>
2023-12-11 14:41:49 +0100

    Complete phase 01

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,7 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  if (typeof string !== "string")
+    throw new Error(
+      `Expected argument of type string, got argument of ${typeof string}`
+    );
+  return string.split("").reverse().join("");
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,12 @@
-// Your code here
+const { expect } = require("chai");
+const reverseString = require("../problems/reverse-string");
+
+describe("reverseString()", () => {
+  it("should return 'nuf' if the argument provided is 'fun'", () => {
+    expect(reverseString("fun")).to.equal("nuf");
+  });
+
+  it("should throw an error of TypeError if argument is not a string", () => {
+    expect(() => reverseString(2)).to.throw(Error);
+  });
+});
