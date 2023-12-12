41d6d323ed251ccb1112b9de30b7c06680bef87d
helcio <h.andre@giap.pl>
2023-12-12 11:12:18 +0100

    Add Triangle class and the getPerimeter method

--- problems/triangle.js
+++ problems/triangle.js
@@ -1,1 +1,13 @@
-// Your code here
+class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  getPerimeter() {
+    return this.side1 + this.side2 + this.side3;
+  }
+}
+
+module.exports = Triangle;
--- test/triangle-spec.js
+++ test/triangle-spec.js
@@ -1,1 +1,18 @@
-// Your code here
+const { expect } = require("chai");
+
+const Triangle = require("../problems/triangle");
+
+describe("Triangle class", () => {
+  beforeEach(() => {
+    triangle = new Triangle(4, 5, 4);
+  });
+  it("should have three sides as keys when initiated", () => {
+    expect(triangle).to.have.keys("side1", "side2", "side3");
+  });
+
+  describe("Triangle.getPerimeter()", () => {
+    it("return the sum of the triangle instance 3 sides when invoked", () => {
+      expect(triangle.getPerimeter()).to.equal(13);
+    });
+  });
+});
